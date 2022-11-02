835e378d66de6c7580d5a9199207730f4fec3859
saqib <saqib29111@gmail.com>
2022-11-02 19:39:28 +0600

    integrate Keycloak with Spring Cloud Gateway

---
+++ spring-gateway-client-application/src/main/java/com/amrut/oarbhu/client/springgatewayclientapplication/SpringGatewayClientApplication.java
@@ -0,0 +1,13 @@
+package com.amrut.oarbhu.client.springgatewayclientapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayClientApplication.class, args);
+	}
+
+}
---
+++ spring-gateway-client-application/src/test/java/com/amrut/oarbhu/client/springgatewayclientapplication/SpringGatewayClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrut.oarbhu.client.springgatewayclientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
